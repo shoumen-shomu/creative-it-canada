8aca1301d448a353c7ad7ab07453b7078189d926
shoumen-shomu <shoumenshomu@gmail.com>
2026-04-08 23:57:47 +0600

    Top Header Done

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({btnTxt,}) => {
   return (
-    <div className={`py-8 px-4 rounded-[41.5px] text-white`}>{btnTxt}</div>
+    <div className={`py-8 px-4 rounded-[41.5px] text-white `}>{btnTxt}</div>
   )
 }
 
--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { FaPhoneAlt } from "react-icons/fa";
+import { IoMail } from "react-icons/io5";
 import Container from "./Container";
 
 const Header = () => {
   return (
     <header className="py-1.75 bg-red-500">
       <Container>
-        <div className="">
+        <div className="flex gap-13">
           <div className="flex gap-2 items-center">
-            <FaPhoneAlt className="text-white" />
-            <p className="text-white">01624888444</p>
+            <FaPhoneAlt className="text-white" /> 
+            <p className="font-hind font-medium text-base text-white leading-6.75">01624888444</p>
+          </div>
+          <div className="flex gap-2 items-center">
+            <IoMail  className="text-white" /> 
+            <p className="font-hind font-medium text-base text-white leading-6.75">help@creativeitinstitute.com</p>
           </div>
         </div>
       </Container>
