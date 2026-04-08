51586d14ffe1af2882f3569b812cb934b27ed41b
shoumen-shomu <shoumenshomu@gmail.com>
2026-04-08 23:53:46 +0600

    Font Family Added

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { FaPhoneAlt } from "react-icons/fa";
+import Container from "./Container";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="py-1.75 bg-red-500">
+      <Container>
+        <div className="">
+          <div className="flex gap-2 items-center">
+            <FaPhoneAlt className="text-white" />
+            <p className="text-white">01624888444</p>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
