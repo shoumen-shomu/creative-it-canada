7e34918792864523e572c1f102147b0c5d8e7582
shoumen-shomu <shoumenshomu@gmail.com>
2026-04-09 23:59:43 +0600

    banner added

--- src/app/components/layouts/home/Banner.jsx
+++ src/app/components/layouts/home/Banner.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Banner = () => {
   return (
     <>
-    
+    <section>
+        <div class="bg-[url('/images/banner.png')] "></div>
+    </section>
+
     </>
   )
 }
