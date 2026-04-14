4fb1f07b53688be292e3dd8c671f0037e4ac3ec4
shoumen-shomu <shoumenshomu@gmail.com>
2026-04-15 00:08:04 +0600

    Feedback Part Done

--- src/app/components/layouts/home/Feedback.jsx
+++ src/app/components/layouts/home/Feedback.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Container from "../../Container";
 import Image from "next/image";
+import Button from "../../Button";
 
 const Feedback = () => {
   return (
@@ -88,11 +89,12 @@ const Feedback = () => {
                   </div>
                   <div className="p-6 bg-white rounded-xl border border-[#D4D4D4]">
                     <p className="font-inter font-normal text-base text-[#605F62] leading-[150%] w-146.25">
-                      Creative It changed my life. They just took a place into
-                      heart. Already few of my friends are student their because
-                      recommend to take courses. Cause they have really
-                      professional trainers to guide you to success. Thanks for
-                      changing my life creative it institute.
+                      Every moment,the static image of many organizations floats
+                      in front of the eyes.But creative IT seems to be the best
+                      from the beginning. By taking classes here,I feel more
+                      fulfilled,I have started seeing new dreams.In a word,I am
+                      having a wonderful time here.I always grateful to Rocky
+                      Sir & Creative IT.
                     </p>
                     <div className="flex gap-4 items-center">
                       <Image
@@ -114,11 +116,10 @@ const Feedback = () => {
                   </div>
                   <div className="p-6 bg-white rounded-xl border border-[#D4D4D4]">
                     <p className="font-inter font-normal text-base text-[#605F62] leading-[150%] w-146.25">
-                      Creative It changed my life. They just took a place into
-                      heart. Already few of my friends are student their because
-                      recommend to take courses. Cause they have really
-                      professional trainers to guide you to success. Thanks for
-                      changing my life creative it institute.
+                      Creative IT Institute is a great place for me to gain
+                      knowledge and learn a lots of things which is unexpectedly
+                      awesome. I love CITI for support me and also thanks for
+                      gave me the chance in the Scholarship.
                     </p>
                     <div className="flex gap-4 items-center">
                       <Image
@@ -139,7 +140,59 @@ const Feedback = () => {
                     </div>
                   </div>
                 </div>
-                <div className=""></div>
+                <div className="space-y-7.5">
+                  <div className="p-6 bg-white rounded-xl border border-[#D4D4D4]">
+                    <p className="font-inter font-normal text-base text-[#605F62] leading-[150%] w-66.75">
+                      Creative IT is one of the top institutes for learning
+                      graphics design; they teach us in a highly informative
+                      manner while also providing a conducive learning
+                      environment.
+                    </p>
+                    <Image
+                      className="pb-4 pt-6"
+                      src="/images/reviewsix.png"
+                      alt="reviewsix"
+                      width={60}
+                      height={60}
+                    />
+                    <h5 className="font-inter font-bold  text-[20px] text-[#000000] leading-[150%]">
+                      Ibadol Islam
+                    </h5>
+                    <p className="font-inter font-normal text-[18px] text-[#7E7E7E] leading-[150%]">
+                      Student of Web Development
+                    </p>
+                  </div>
+                  <div className="p-6 bg-white rounded-xl border border-[#D4D4D4]">
+                    <p className="font-inter font-normal text-base text-[#605F62] leading-[150%] w-66.75">
+                      I like Creative IT very much because here the teachers
+                      explain it very nicely and if I face any problem they
+                      solve my problem very easily & quickly and also their
+                      communication skill is very good. Creative IT is the best
+                      institute from any other institute.
+                    </p>
+                    <Image
+                      className="pb-4 pt-6"
+                      src="/images/reviewseven.png"
+                      alt="reviewseven"
+                      width={60}
+                      height={60}
+                    />
+                    <h5 className="font-inter font-bold  text-[20px] text-[#000000] leading-[150%]">
+                      Shaon Sarker
+                    </h5>
+                    <p className="font-inter font-normal text-[18px] text-[#7E7E7E] leading-[150%]">
+                      Student of Web Development
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="text-center mt-15">
+                <Button
+                  className={
+                    "font-inter font-medium text-[18px] border border-primary hover:border-primary hover:bg-white hover:text-[#CF0000]  duration-300"
+                  }
+                  btnTxt={"See All Feedback"}
+                />
               </div>
             </div>
           </Container>
