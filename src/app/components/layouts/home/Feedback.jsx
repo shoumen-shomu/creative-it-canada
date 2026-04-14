2048892a23c3b90af2f7cac04d495c80663573b3
shoumen-shomu <shoumenshomu@gmail.com>
2026-04-14 23:29:25 +0600

    Feedback Heading Added

--- src/app/components/layouts/home/Feedback.jsx
+++ src/app/components/layouts/home/Feedback.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Container from '../../Container'
 
 const Feedback = () => {
   return (
-    <div>Feedback</div>
+    <>
+    <section>
+      <div className="mt-25 bg-[#FBFBFB]">
+        <Container>
+          <div className="pt-25 text-center">
+            <h2 className='font-inter font-bold text-[48px] text-[#1F1E1E] leading-[120%]'>Our Clients <span className='font-tri font-bold text-[48px] text-primary leading-[120%] italic'>Feedback</span></h2>
+          </div>
+
+        </Container>
+      </div>
+        
+    </section>
+    </>
   )
 }
 
