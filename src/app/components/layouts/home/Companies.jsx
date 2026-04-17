38402012c863aadf03478e173f00cac8d939bcd0
shoumen-shomu <shoumenshomu@gmail.com>
2026-04-17 20:18:57 +0600

    Header Added

--- src/app/components/layouts/home/Companies.jsx
+++ src/app/components/layouts/home/Companies.jsx
@@ -4,7 +4,11 @@ const Companies = () => {
   return (
     <>
     <section className='my-25'>
-        
+        <div className="text-center">
+            <h2 className='font-tri font-bold text-[48px] text-primary leading-[150%] italic'>700+ Companies</h2>
+            <p className='font-inter font-normal text-[32px] text-[#1F1E1E] leading-[150%] tracking-[0.2px]'>Are Connected to Us</p>
+        </div>
+
 
     </section>
     </>
