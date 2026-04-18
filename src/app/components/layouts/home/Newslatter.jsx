1765053ab9f307ca38a37d7f9a8d11b839233d26
shoumen-shomu <shoumenshomu@gmail.com>
2026-04-18 23:38:55 +0600

    newsletter heading added

--- src/app/components/layouts/home/Newslatter.jsx
+++ src/app/components/layouts/home/Newslatter.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React from "react";
+import Container from "../../Container";
 
 const Newslatter = () => {
   return (
     <>
-    <section className='mt-[182px]'>
-
-    </section>
+      <section className="mt-45.5">
+        <Container>
+          <div className="p-25 items-center bg-[#FFF7EB] rounded-[48px]">
+            <h2 className="font-inter font-bold text-[60px] text-[#000000] leading-[110%] tracking-[0.2px]">
+              Get in Touch <br />
+              <span className="font-tri font-bold text-[60px] text-primary leading-[110%] tracking-[0.2px] italic">
+                with Us!
+              </span>
+            </h2>
+          </div>
+        </Container>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Newslatter
+export default Newslatter;
