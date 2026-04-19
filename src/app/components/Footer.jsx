b265f235b0d14125e40335a6931af97e2e7db24f
shoumen-shomu <shoumenshomu@gmail.com>
2026-04-20 00:13:21 +0600

    justify between added

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
   return (
     <footer className="pt-93 pb-10 bg-[#1F1E1E]">
       <Container>
-        <div className="flex gap-31">
+        <div className="flex justify-between">
           <div className="space-y-7.5">
             <h4 className="font-inter font-bold text-[21px] text-white leading-8.25 tracking-[0.2px] ">
               Contact Us
