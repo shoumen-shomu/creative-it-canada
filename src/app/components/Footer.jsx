21545527ae0ae0748b629b7a2ff9be3da5307fe6
shoumen-shomu <shoumenshomu@gmail.com>
2026-04-19 23:37:20 +0600

    some Components Added

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,10 +1,18 @@
 'use client';
 import React from 'react'
+import Container from './Container';
 
 const Footer = () => {
   return (
 
     <footer className='pt-93 pb-10 bg-[#1F1E1E]'>
+      <Container>
+        <div className="flex">
+          <div className="">
+            <h4 className='font-inter font-bold text-[21px] text-white leading-8.25 tracking-[0.2px] '>Contact Us</h4>
+          </div>
+        </div>
+      </Container>
 
     </footer>
     
