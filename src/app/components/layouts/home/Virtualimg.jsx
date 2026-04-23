83da9f4da5ca60e773d408bab7eef6da824c554c
shoumen-shomu <shoumenshomu@gmail.com>
2026-04-24 00:05:55 +0600

    Virtual Image px  added

--- src/app/components/layouts/home/Virtualimg.jsx
+++ src/app/components/layouts/home/Virtualimg.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 const Virtualimg = () => {
   return (
     <section>
-        <div className='py-25 bg-[#FBFBFB]'>
+        <div className='px-2 lg:px-0 py-25 bg-[#FBFBFB]'>
             <Container>
                 <Image src="/images/virtualimg.png" alt='virtualimg.png' width={1320} height={720}/>
             </Container>
