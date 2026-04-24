dfe7b1b81a0c91facaaac52a1ea558048db2206a
shoumen-shomu <shoumenshomu@gmail.com>
2026-04-24 23:53:56 +0600

    Companies Part responsive done

--- src/app/components/layouts/home/Companies.jsx
+++ src/app/components/layouts/home/Companies.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 const Companies = () => {
   return (
     <>
-      <section className="mt-25 mb-100">
+      <section className="mt-25 mb-100 px-2 lg:px-0">
         <Container>
           <div className="text-center">
             <h2 className="font-tri font-bold lg:text-[48px] text-[30px] text-primary leading-[150%] italic">
@@ -15,27 +15,27 @@ const Companies = () => {
               Are Connected to Us
             </p>
           </div>
-          <div className="mt-14.5 p-2 border-[#D9D9D9] border rounded-[37px] flex lg:w-200 w-full mx-auto items-center">
-            <div className="group py-4 lg:px-16 px-2 bg-white rounded-[29px] hover:bg-primary duration-300 cursor-pointer transition-all">
+          <div className="mt-14.5 p-2 border-[#D9D9D9] border rounded-[37px] flex lg:w-200  mx-auto items-center">
+            <div className="group py-4 lg:px-16 px-3.5 bg-white rounded-[29px] hover:bg-primary duration-300 cursor-pointer transition-all">
               <p className="font-inter font-semibold lg:text-[24px] text-base text-[#000000] group-hover:text-white leading-tight tracking-[0.2px] whitespace-nowrap">
                 Working with
               </p>
             </div>
 
-            <div className="group py-4 lg:px-16 px-2 bg-white rounded-[29px] hover:bg-primary duration-300 cursor-pointer transition-all">
+            <div className="group py-4 lg:px-16 px-3.5 bg-white rounded-[29px] hover:bg-primary duration-300 cursor-pointer transition-all">
               <p className="font-inter font-semibold lg:text-[24px] text-base text-[#000000] group-hover:text-white leading-tight tracking-[0.2px] whitespace-nowrap">
                 Members of
               </p>
             </div>
 
-            <div className="group py-4 lg:px-16 px-2 bg-white rounded-[29px] hover:bg-primary duration-300 cursor-pointer transition-all">
+            <div className="group py-4 lg:px-16 px-3.5 bg-white rounded-[29px] hover:bg-primary duration-300 cursor-pointer transition-all">
               <p className="font-inter font-semibold lg:text-[24px] text-base text-[#000000] group-hover:text-white leading-tight tracking-[0.2px] whitespace-nowrap">
                 Our Concerns
               </p>
             </div>
           </div>
           <Container>
-            <div className="mt-25">
+            <div className="lg:mt-25 mt-15">
               <Image src="/images/allinstitute.png" alt="allinstitute" width={1313} height={425}/>
             </div>
           </Container>
